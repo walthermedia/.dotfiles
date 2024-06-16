245403c733bb8ef04264ecae9a28fb789be442ec
Ben <119703246+walthermedia@users.noreply.github.com>
2024-06-16 16:13:37 -0500

    Initial commit
    New stuff

---
+++ .zsh/config/aliases.zsh
@@ -0,0 +1,9 @@
+# Aliases
+
+# ---- switched to abbreviations ------------
+# alias gst="git status"
+# alias yst="yadm status"
+# alias grep='grep --color'
+
+alias ls="exa --icons --group-directories-first -l"
+alias ll="exa --icons --group-directories-first -la"
